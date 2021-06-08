cc90c13286e8e90669b6131670b5fa354f800507
vaibhavnshah <vaibhavshah7777@gmail.com>
2021-06-08 18:07:00 -0500

    File Recognition, Validation, and Language Inspection
    Take command line input for file and get path, then validate that it exists, then get language of file input

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/askgitdev/tsparse
 
 go 1.16
+
+require (
+	github.com/go-enry/go-enry/v2 v2.7.0 // indirect
+	rsc.io/quote v1.5.2 // indirect
+)
--- tsparse.go
+++ tsparse.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/go-enry/go-enry/v2"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	flag.Parse()
+	path := flag.Arg(0)
+	absPath, _ := filepath.Abs(path)
+	fmt.Println("Path to file: " + absPath)
+	f, err := exists(absPath)
+	if !f {
+		fmt.Println("There was either an error in the command line input or a faulty filepath. Please try again.")
+		log.Fatal(err)
+	}
+	contents, _ := ioutil.ReadFile(absPath)
+	lang := enry.GetLanguage(absPath, []byte(contents))
+	fmt.Println("language: " + lang)
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
