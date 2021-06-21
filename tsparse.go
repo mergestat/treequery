abf46b912af0d1d78532b5e6f3540181d42d1f59
vaibhavnshah <vaibhavshah7777@gmail.com>
2021-06-21 11:25:41 -0500

    testing other langs
    changed javascript.getLanguage() on line 46 to java, which should allow java file inputs; however, "panic: runtime error: invalid memory address or nil pointer dereference" is returned

--- tsparse.go
+++ tsparse.go
@@ -33,7 +33,34 @@ func main() {
 	parser.SetLanguage(javascript.GetLanguage())
 	tree := parser.Parse(nil, contents)
 	n := tree.RootNode()
+<<<<<<< Updated upstream
 	fmt.Println(n)
+=======
+	fmt.Println("language: " + lang)
+	fmt.Println("AST:", n)
+
+	fmt.Println("Root type:", n.Type())
+	fmt.Println("Root children:", n.ChildCount())
+
+	fmt.Println("\nFunctions in input:")
+	q, _ := sitter.NewQuery([]byte("(function_declaration) @func"), java.GetLanguage())
+	qc := sitter.NewQueryCursor()
+	qc.Exec(q, n)
+
+	var funcs []*sitter.Node
+	for {
+		m, ok := qc.NextMatch()
+		if !ok {
+			break
+		}
+
+		for _, c := range m.Captures {
+			funcs = append(funcs, c.Node)
+			fmt.Println("-", funcName(contents, c.Node))
+		}
+	}
+	fmt.Println(funcs)
+>>>>>>> Stashed changes
 }
 
 func exists(path string) (bool, error) {
@@ -46,3 +73,42 @@ func exists(path string) (bool, error) {
 	}
 	return false, err
 }
+<<<<<<< Updated upstream
+=======
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func getTSParser(lang string) sitter.Parser {
+	parser := sitter.NewParser()
+	switch strings.ToLower(lang) {
+	case "javascript":
+		parser.SetLanguage(javascript.GetLanguage())
+	case "go":
+		parser.SetLanguage(golang.GetLanguage())
+	case "python":
+		parser.SetLanguage(python.GetLanguage())
+	case "java":
+		parser.SetLanguage(java.GetLanguage())
+	default:
+		handleErr(errors.New("language not supported at this time"))
+	}
+	return *parser
+}
+
+func funcName(content []byte, n *sitter.Node) string {
+	if n == nil {
+		return ""
+	}
+
+	if n.Type() != "function_declaration" {
+		return ""
+	}
+
+	return n.ChildByFieldName("name").Content(content)
+}
+>>>>>>> Stashed changes
