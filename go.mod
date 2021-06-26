82650c00d98590791563fbab062c1364326e18a8
vaibhavnshah <vaibhavshah7777@gmail.com>
2021-06-26 14:20:11 -0500

    Method Querying  (#4)
    * tree sitter JS integration
    
    put in an example tree sitter implementation
    
    * testing other langs
    
    changed javascript.getLanguage() on line 46 to java, which should allow java file inputs; however, "panic: runtime error: invalid memory address or nil pointer dereference" is returned
    
    * update to testing
    
    messing w changes
    
    * updated gitignore and added examples
    
    in title
    
    * searches for and prints method content
    
    automatic language detection and grammar retrieval now fully integrated into the querying, added err handling removed in bad merge back, now prints file name and line numbers before function content for each function
    
    * clickable locations and error handling update
    
    the path to file and line number can now be clicked
    
    * gettsparser update
    
    lang string input no longer edited
    
    * Update tsparse.go
    
    Co-authored-by: Patrick DeVivo <patrick.devivo@gmail.com>
    
    * Update tsparse.go
    
    Co-authored-by: Patrick DeVivo <patrick.devivo@gmail.com>
    
    * grammar initialization
    
    changed grammar initialization back to using new()
    
    * changed folder name, minor err handling
    
    Co-authored-by: Patrick DeVivo <patrick.devivo@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/askgitdev/tsparse
 
 go 1.16
 
-require github.com/go-enry/go-enry/v2 v2.7.0
+require (
+	github.com/go-enry/go-enry/v2 v2.7.0
+	github.com/smacker/go-tree-sitter v0.0.0-20210519070246-0ba5d9f5237b
+)
--- tsparse.go
+++ tsparse.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/go-enry/go-enry/v2"
+	sitter "github.com/smacker/go-tree-sitter"
+	"github.com/smacker/go-tree-sitter/golang"
+	"github.com/smacker/go-tree-sitter/java"
+	"github.com/smacker/go-tree-sitter/javascript"
+	"github.com/smacker/go-tree-sitter/python"
 )
 
 func main() {
@@ -16,14 +22,46 @@ func main() {
 	path := flag.Arg(0)
 	absPath, _ := filepath.Abs(path)
 	fmt.Println("Path to file: " + absPath)
-	f, err := exists(absPath)
+	f, _ := exists(absPath)
 	if !f {
-		fmt.Println("There was either an error in the command line input or a faulty filepath. Please try again.")
-		log.Fatal(err)
+		handleErr(errors.New("there was either an error in the command line input or a faulty filepath"))
 	}
-	contents, _ := ioutil.ReadFile(absPath)
-	lang := enry.GetLanguage(absPath, []byte(contents))
+
+	contents, readErr := ioutil.ReadFile(absPath)
+	handleErr(readErr)
+	lang := enry.GetLanguage(absPath, contents)
 	fmt.Println("language: " + lang)
+	if lang == "" {
+		handleErr(errors.New("language could not be detected"))
+	}
+	parser, grammar := getTSParser(lang)
+	tree := parser.Parse(nil, contents)
+	n := tree.RootNode()
+
+	fmt.Println("AST:", n)
+
+	fmt.Println("Root type:", n.Type())
+	fmt.Println("Root children:", n.ChildCount())
+
+	fmt.Println("\nFunctions in input:")
+	q, errQuery := sitter.NewQuery([]byte("(method_declaration) @func"), &grammar)
+	handleErr(errQuery)
+	qc := sitter.NewQueryCursor()
+	qc.Exec(q, n)
+
+	var funcs []*sitter.Node
+	for {
+		m, ok := qc.NextMatch()
+		if !ok {
+			break
+		}
+
+		for _, c := range m.Captures {
+			funcs = append(funcs, c.Node)
+			fmt.Println("-", absPath+":"+strconv.FormatUint(uint64(c.Node.StartPoint().Row), 10), "-", c.Node.EndPoint().Row)
+			fmt.Println(c.Node.Content(contents))
+		}
+	}
 }
 
 func exists(path string) (bool, error) {
@@ -35,4 +73,38 @@ func exists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func getTSParser(lang string) (sitter.Parser, sitter.Language) {
+	parser := sitter.NewParser()
+	grammar := new(sitter.Language)
+	switch lang {
+	case "JavaScript":
+		grammar = javascript.GetLanguage()
+	case "Go":
+		grammar = golang.GetLanguage()
+	case "Python":
+		grammar = python.GetLanguage()
+	case "Java":
+		grammar = java.GetLanguage()
+	default:
+		handleErr(errors.New("language not supported at this time"))
+	}
+	parser.SetLanguage(grammar)
+	return *parser, *grammar
+}
+
+func funcName(content []byte, n *sitter.Node) string {
+	if n == nil {
+		return ""
+	}
+
+	return n.ChildByFieldName("name").Content(content)
 }
