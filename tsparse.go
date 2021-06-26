38ce3aa4f079b12c30cc71e72374e7c39f29ef54
Patrick DeVivo <patrick.devivo@gmail.com>
2021-06-26 15:52:35 -0400

    some cleaning up and rearranging

--- tsparse.go
+++ tsparse.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strconv"
 
 	"github.com/go-enry/go-enry/v2"
 	sitter "github.com/smacker/go-tree-sitter"
@@ -17,55 +16,14 @@ import (
 	"github.com/smacker/go-tree-sitter/python"
 )
 
-func main() {
-	flag.Parse()
-	path := flag.Arg(0)
-	query := flag.Arg(1)
-	absPath, _ := filepath.Abs(path)
-	fmt.Println("Path to file: " + absPath)
-	f, _ := exists(absPath)
-	if !f {
-		handleErr(errors.New("there was either an error in the command line input or a faulty filepath"))
-	}
-
-	contents, readErr := ioutil.ReadFile(absPath)
-	handleErr(readErr)
-	lang := enry.GetLanguage(absPath, contents)
-	fmt.Println("language: " + lang)
-	if lang == "" {
-		handleErr(errors.New("language could not be detected"))
-	}
-	parser, grammar := getTSParser(lang)
-	tree := parser.Parse(nil, contents)
-	n := tree.RootNode()
-
-	fmt.Println("AST:", n)
-
-	fmt.Println("Root type:", n.Type())
-	fmt.Println("Root children:", n.ChildCount())
-
-	fmt.Println("\nFunctions in input:")
-	q, errQuery := sitter.NewQuery([]byte(query), &grammar)
-	handleErr(errQuery)
-	qc := sitter.NewQueryCursor()
-	qc.Exec(q, n)
-
-	var funcs []*sitter.Node
-	for {
-		m, ok := qc.NextMatch()
-		if !ok {
-			break
-		}
-
-		for _, c := range m.Captures {
-			funcs = append(funcs, c.Node)
-			fmt.Println("-", absPath+":"+strconv.FormatUint(uint64(c.Node.StartPoint().Row), 10), "-", c.Node.EndPoint().Row)
-			fmt.Println(c.Node.Content(contents))
-		}
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
 
-func exists(path string) (bool, error) {
+func fileExists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
 		return true, nil
@@ -76,28 +34,73 @@ func exists(path string) (bool, error) {
 	return false, err
 }
 
-func handleErr(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+// getTSLanguageFromEnry retrieves the tree sitter language from a language name string (defined by the enry package)
+func getTSLanguageFromEnry(lang string) *sitter.Language {
+	switch lang {
+	case "JavaScript":
+		return javascript.GetLanguage()
+	case "Go":
+		return golang.GetLanguage()
+	case "Python":
+		return python.GetLanguage()
+	case "Java":
+		return java.GetLanguage()
+	default:
+		return nil
 	}
 }
 
-func getTSParser(lang string) (sitter.Parser, sitter.Language) {
-	parser := sitter.NewParser()
-	grammar := new(sitter.Language)
-	switch lang {
-	case "JavaScript":
-		grammar = javascript.GetLanguage()
-	case "Go":
-		grammar = golang.GetLanguage()
-	case "Python":
-		grammar = python.GetLanguage()
-	case "Java":
-		grammar = java.GetLanguage()
-	default:
-		handleErr(errors.New("language not supported at this time"))
+func main() {
+	flag.Parse()
+	path := flag.Arg(0)
+	query := flag.Arg(1)
+
+	absPath, err := filepath.Abs(path)
+	handleErr(err)
+
+	f, err := fileExists(absPath)
+	handleErr(err)
+
+	if !f {
+		handleErr(errors.New("file does not exist"))
 	}
-	parser.SetLanguage(grammar)
-	return *parser, *grammar
+
+	contents, err := ioutil.ReadFile(absPath)
+	handleErr(err)
+
+	lang := enry.GetLanguage(absPath, contents)
+	if lang == "" {
+		handleErr(errors.New("language could not be detected"))
+	}
+
+	language := getTSLanguageFromEnry(lang)
+	parser := sitter.NewParser()
+	parser.SetLanguage(language)
+
+	tree := parser.Parse(nil, contents)
+	n := tree.RootNode()
+
+	if query == "" {
+		// fmt.Println(absPath)
+		// fmt.Println(n.Content(contents))
+		fmt.Println(n)
+	}
+
+	q, err := sitter.NewQuery([]byte(query), language)
+	handleErr(err)
+
+	qc := sitter.NewQueryCursor()
+	qc.Exec(q, n)
+
+	for {
+		m, ok := qc.NextMatch()
+		if !ok {
+			break
+		}
+
+		for _, c := range m.Captures {
+			fmt.Println(absPath + ":" + fmt.Sprintf("%d", c.Node.StartPoint().Row+1))
+			fmt.Println(c.Node.Content(contents))
+		}
+	}
 }
